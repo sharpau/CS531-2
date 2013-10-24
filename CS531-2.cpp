447f235e5930b7bc2287a48530155b56f1f07b17
Austin Sharp <austinsharp111@gmail.com>
2013-10-24 15:07:53 -0700

    state class coming together yo

--- CS531-2.cpp
+++ CS531-2.cpp
@@ -5,11 +5,42 @@
 
 struct state {
 	// each vector is a peg. index 0 is the bottom, and index size-1 is the top, accessible
-	std::vector<int> A;
-	std::vector<int> B;
-	std::vector<int> C;
+	std::vector<std::vector<int>> pegs;
 
-	state(int n, int problem) {
+	int mDisks;
+
+	bool isGoal(void) {
+		for(int i = 0; i < pegs[0].size(); i++) {
+			if(pegs[0][i] != i) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+
+
+	std::vector<state> successors(void) {
+		std::vector<state> succs;
+		for(int i = 0; i < pegs.size(); i++) {
+			if(pegs[i].size() > 0) {
+				for(int j = 0; j < pegs.size(); j++) {
+					if(i != j) {
+						state new_state_1 = *this;
+						new_state_1.pegs[j].push_back(new_state_1.pegs[i].back());
+						new_state_1.pegs[i].pop_back();
+
+						succs.push_back(new_state_1);
+					}
+				}
+			}
+		}
+		return succs;
+	};
+
+	state(const state &copy) : pegs(copy.pegs), mDisks(copy.mDisks) {};
+
+	state(int n, int problem) : mDisks(n) {
 		// hard-coded problems for different sizes
 		static const int size4[20][4] = {
 			{2,0,1,3},
@@ -99,35 +130,46 @@ struct state {
 			{4,7,9,6,2,5,1,0,3,8},
 			{3,6,8,5,7,9,0,1,2,4}};
 
+		for(int i = 0; i < 3; i++) {
+			pegs.push_back(std::vector<int>());
+		}
+
 		// initialize A with the selected size and problem
 		switch(n) {
 		case 4: 
 			for(int i = 0; i < 4; i++) {
-				A.push_back(size4[problem][i]);
+				pegs[0].push_back(size4[problem][i]);
 			}
 			break;
 		case 6: 
 			for(int i = 0; i < 6; i++) {
-				A.push_back(size6[problem][i]);
+				pegs[0].push_back(size6[problem][i]);
 			}
 			break;
 		case 8: 
 			for(int i = 0; i < 8; i++) {
-				A.push_back(size8[problem][i]);
+				pegs[0].push_back(size8[problem][i]);
 			}
 			break;
 		case 10: 
 			for(int i = 0; i < 10; i++) {
-				A.push_back(size10[problem][i]);
+				pegs[0].push_back(size10[problem][i]);
 			}
 			break;
 		}
 	};
 };
 
+int heuristicAdmissible(state current) {
+	// number of disks in the incorrect location
+	return 0;
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	state test = state(4, 4);
+	auto succs = test.successors();
 	return 0;
 }
 
